351327908f0fd554c75974d9e68e34aaf35fac00
Jonys2001 <escobedo.jonathan@uabc.edu.mx>
2023-10-10 19:32:18 +0200

    Se comparan numeros

---
+++ practica_26/practica_26.js
@@ -0,0 +1,59 @@
+/*
+var num = 8;
+
+if (num < 10 ) {
+    console.log("El numero es menor a 10")
+} else {
+    console.log("El numero es mayor a 10")
+}
+*/
+/*
+var num = 8;
+
+if (num < 10 ) {
+    console.log("El numero es menor a 10")
+} else if (num > 10 ) {
+    console.log("El numero es mayor a 10")
+}else{
+    console.log("El numero es igual a 10")
+}
+*/
+
+/* 
+var num = prompt("Escribe una estacion en minusculas");
+
+switch (num) {
+    case "invierno":
+            alert("Todo es frio");
+        break;
+    case "primavera":
+            alert("Se colorea el mundo");
+        break;
+    case "verano":
+            alert("Se colorea el mundo");
+         break;
+    case "otoño":
+            alert("Se colorea el mundo");
+         break;
+
+    default: alert("Eso no es una estacion");
+        break;
+}
+*/
+
+
+var num1 = 5;
+var num2 = 8;
+
+if (num1 <= num2 ) {
+
+    alert(num1 + " no es mayor a " + num2 );
+    
+} if (num2 >= 0 ) {
+   alert(num2 + " es positivo " );
+
+} if( ++num1 < num2){
+    alert(" incrementar uno a  " + num1 + " no lo hace mayor a " 
+    + num2 );
+
+}
