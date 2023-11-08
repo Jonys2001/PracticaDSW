34ea86af2584b493062d748c3633a8ef56524221
Jonys2001 <escobedo.jonathan@uabc.edu.mx>
2023-11-08 09:08:15 -0800

    vimos funciones

---
+++ practica_30/practica_30.js
@@ -0,0 +1,18 @@
+var num1 = parseInt(prompt("Ingrese un numero"));
+
+function parimpar(num1) {
+    var num = num1 % 2;
+    if (num == 0 ) {
+            alert("tu numero "+ num1 +" es par");
+    }else{ 
+            alert("tu numero "+ num1 +" es impar");
+    }
+}
+ parimpar();
+
+
+
+
+
+
+ 5
